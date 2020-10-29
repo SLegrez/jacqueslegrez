dd6bd7d509c055fee4a00403b004b6c3d64f0a3a
stanislas legrez <stanislas27@gmail.com>
2020-10-29 15:52:17 +0200

    Added new pages in carousels, new pics, began contact page

--- app/javascript/components/typed.js
+++ app/javascript/components/typed.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Regarder pour voir"],
+    strings: ["« Regarder pour voir »"],
     typeSpeed: 50,
     loop: true,
     backSpeed: 50
