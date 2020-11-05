51738aa0b40a2c3ea468a94c3f5251bb49a4d29e
stanislas legrez <stanislas27@gmail.com>
2020-11-05 16:13:19 +0200

    resolved carousel <bug>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,9 +16,7 @@ import 'bootstrap';
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import { loadDynamicBannerText } from '../components/typed';
-import { checkActiveModal } from '../components/activeModal';
 
 document.addEventListener('turbolinks:load', () => {
   loadDynamicBannerText();
-  checkActiveModal();
 });
