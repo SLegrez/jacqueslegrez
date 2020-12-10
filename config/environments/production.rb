584b98520c8e22ec90967cecae2f6c0cdb57907b
stanislas legrez <stanislas27@gmail.com>
2020-12-10 15:07:17 +0200

    new one

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'https://jacques-legrez.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'www.jacqueslegrez.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
@@ -126,7 +126,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'https://jacques-legrez.herokuapp.com/',
+    :domain => 'www.jacqueslegrez.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
