c1acc68eeecf2c67d73e0b88c6532bf7b1026d95
stanislas legrez <stanislas27@gmail.com>
2021-01-06 13:20:50 +0100

    pics work but with ?locale=fr and not with /fr. But for now it makes the job

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  scope "(:locale)", locale: /fr|en/ do
+  # scope "(:locale)", locale: /fr|en/ do
+    get '/:locale' => 'pages#home'
     root to: "pages#home"
     get 'lart-en-question', to: 'pages#lart_en_question'
     get 'contact', to: 'pages#contact'
     get 'oeuvres/:id', to: 'oeuvres#index', as: 'oeuvres_collection_index'
     resources :contacts, only: [:new, :create]
     post 'contact', to: 'contacts#create'
-  end
+  # end
 end
