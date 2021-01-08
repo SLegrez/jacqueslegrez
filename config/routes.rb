eb5be7728b2b68e132df9c49b1b5469756d1c5d7
stanislas legrez <stanislas27@gmail.com>
2021-01-08 15:32:01 +0100

    root path redirect to default locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: redirect("/#{I18n.default_locale}"), as: :redirected_root
   scope "(:locale)", locale: /fr|en/ do
     root to: "pages#home"
     get 'lart-en-question', to: 'pages#lart_en_question'
