eb9f7c68880e14f638bce60134c45ce04b004c48
stanislas legrez <stanislas27@gmail.com>
2021-01-26 14:39:09 +0100

    translated typed.js for internalization

--- app/javascript/components/typed.js
+++ app/javascript/components/typed.js
@@ -1,9 +1,16 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  if (document.getElementById('banner-typed-text')) {
-    new Typed('#banner-typed-text', {
+  if (document.getElementById('banner-typed-text-fr')) {
+    new Typed('#banner-typed-text-fr', {
       strings: ["« Regarder pour voir »"],
+      typeSpeed: 50,
+      loop: true,
+      backSpeed: 50
+    })
+  } else {
+    new Typed('#banner-typed-text-en', {
+      strings: ["« Look to see »"],
       typeSpeed: 50,
       loop: true,
       backSpeed: 50
