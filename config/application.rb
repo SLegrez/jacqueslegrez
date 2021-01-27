479ece003f6c6aef2db752838cbd24aaad80f081
stanislas legrez <stanislas27@gmail.com>
2021-01-27 13:09:45 +0100

    changed default locale fallback, added in comment method to not display /fr in urls in appcontroller and routes

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Jacqueslegrez
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
     config.i18n.default_locale = :fr
     I18n.available_locales = [:fr, :en]
-    config.i18n.fallbacks = [:fr]
+    config.i18n.fallbacks = [:en]
 
     config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
